86441e1fd2be7f7f3fb97d52391baf14bc88a531
Aleksandr-front-end-developer <78820201+Aleksandr-front-end-developer@users.noreply.github.com>
2023-11-30 19:43:33 +0000

    Deploying to gh-pages from @ Aleksandr-front-end-developer/goit-js-hw-09@35fb1a7a067236a310dc0754b125f4e2f5057e37 🚀

---
+++ 01-color-switcher.0117c7eb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.disabled=!0;var a=null;function d(){document.body.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)))}t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,d(),a=setInterval(d,1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.0117c7eb.js.map
